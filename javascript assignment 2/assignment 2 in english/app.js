ff7a14895c7344878267ac784de4acda42084608
vikash123765 <92863236+vikash123765@users.noreply.github.com>
2021-11-30 16:37:23 +0100

    Add files via upload

---
+++ javascript assignment 2/assignment 2 in english/app.js
@@ -0,0 +1,157 @@
+/*
+Task 1:
+Use the variable blue to "toggle" the background color between blue and white
+for the div with id answer-one when you click the button
+*/
+
+let blue = false;
+
+function taskOne () {
+    alert ("Replace this alert with a solution");
+ 
+}
+
+/*
+Task 2:
+Each time you click on the button, a new random value appears
+is added to the numbers array, before the array is rendered in the div with id answer-two
+*/
+
+const numbers = [234, 986, 784];
+
+function taskTwo () {
+  alert ("Replace this alert with a solution");
+}
+
+/*
+Task 3:
+Identify which day of the week it is, if it is a weekend (Saturday or Sunday)
+you should trigger an alert with the message "Woohooo it's weekend" otherwise about it
+is not a weekend, you should trigger an alert with the message "FML"
+*/
+
+function taskThree () {
+  alert ("Replace this alert with a solution");
+}
+
+/*
+Task 4:
+Call the multiplier function and provide it with two arguments (values), the multiplier should then
+check if these values ​​are numbers, if so the values ​​should be multiplied and the result
+should then be displayed in an alert box along with the following: The product is: [RESULT], otherwise if one of
+or both values ​​are not a number, an alert box with the message "I can only multiply numbers" should be triggered
+*/
+
+function taskFour () {
+    alert ("Replace this alert with a solution");
+}
+
+/*
+Task 5:
+Start by iterating over the array of fruits, if the value is "orange" or "pear"
+you should push the value to the trash array, otherwise you should push the value to the eatable array,
+you should then render out both arrays in the dive "answer-five", where the eatable array should be
+prefixed by "Edible:" in bold and the trash array values ​​should be prefixed by "Rubbish:" in bold
+
+So it will look like this
+Edible: banana, apple, lemon
+Junk: orange, pear
+*/
+
+const fruits = ["banana", "apple", "lemon", "orange", "pear"];
+const eatable = [];
+const trash = [];
+
+function taskFive () {
+  alert ("Replace this alert with a solution");
+}
+
+/*
+Task 6:
+Start by iterating over the person's array, if the person's age attribute is over or equal to 30
+you should push the person's name to the overThirty array, otherwise you should push the value to the underThirty array,
+you should then check if the person's married attribute is true or false, if it's true you should push the person's
+name of the married array, otherwise push the person's name into the notMarried array.
+
+You should then render all the arrays in the dive "answer-six", where the overThirty array values ​​should
+prefixed by "Over 30:" in bold, the values ​​of the Thirty array should be prefixed by "Under 30:" in bold,
+the values ​​of the married array should be prefixed by "Married:" in bold and notMarried array values ​​should be prefixed by "Unmarried:" in bold
+
+So it will look like this
+Over 30: Christian
+Under 30: Diana, Carl, Karin
+Gift: Christian, Diana
+Unmarried: Carl, Karin
+*/
+
+const persons = [
+  {name: "Christian", age: 31, married: true},
+  {name: "Diana", age: 29, married: true},
+  {name: "Carl", age: 28, married: false},
+  {name: "Karin", age: 29, married: false},
+];
+
+const married = [];
+const notMarried = [];
+const overThirty = [];
+const underThirty = [];
+
+function taskSix () {
+  alert ("Replace this alert with a solution");
+}
+
+/*
+Task 7:
+Divide the addMeSomeLineBreaks string at each comma and type
+print the text on a new line in the dive "answer-seven"
+*/
+
+const addMeSomeLineBreaks =
+  "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sed autperit sit aspern consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. , quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?
+
+function taskSeven () {
+  alert ("Replace this alert with a solution");
+}
+
+/*
+Task 8:
+Hide the div with id card-eight for 3 seconds after clicking the button,
+after three seconds, the dive should reappear as usual
+*/
+
+function taskEight () {
+  alert ("Replace this alert with a solution");
+}
+
+/*
+Task 9:
+If the time is over 17.00, the background color must be changed on all divas with the class
+answer-container to blue, otherwise it should be changed to red
+*/
+
+function taskNine () {
+  alert ("Replace this alert with a solution");
+}
+
+/*
+Task 10:
+The last task is to build a calculator by calling and supplying
+the calculator function with three arguments (see the taskTen function),
+in the calculator function, you should then make different calculations (plus, minus, times, divided by) on the two
+the first parameters depending on what the third parameter is set to ("add", "subtract", "multiply", "divide").
+When the result has been calculated, this should be displayed in an alert box.
+
+In addition, before the calculation is made, check that arguments 1 and 2 are set to numbers
+and that argument number three is either set to "add", "subtract", "multiply" or "divide",
+if any of these conditions are not met, display an alert box with the text "Something is wrong"
+*/
+
+function calculator (valueOne, valueTwo, operator) {
+  alert ("Replace this alert with a solution");
+}
+
+function taskTen () {
+  // the first and second arguments should be numbers, the third argument should be
+  // be a string with any of the values ​​"add", "subtract", "multiply", "divide"
+  calculator ();
+}
